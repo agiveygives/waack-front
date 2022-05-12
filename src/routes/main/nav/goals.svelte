da971aa2153629bb6868a3cf87d3e40fd831b63b
agiveygives <JustinCase25XXV@gmail.com>
2022-05-12 16:36:50 -0400

    Goals Select and Add button
    add padding to layout content
    
    select, button, and some styles
    
    base daily goals component

--- src/routes/main/nav/goals.svelte
+++ src/routes/main/nav/goals.svelte
@@ -1,1 +1,50 @@
-<h1>Goals</h1>
+<script lang="ts">
+  import Select, { Option } from '@smui/select';
+  import Button, { Icon, Label } from '@smui/button';
+  import { Daily } from '@components/GoalsCalendar';
+
+  let calendarOptions = [
+    'Daily',
+    'Weekly',
+    'Monthly'
+  ];
+
+  let value = calendarOptions[0];
+</script>
+
+<div class='header'>Goals</div>
+<span>
+  <Select bind:value>
+    {#each calendarOptions as option}
+      <Option value={option}>{option}</Option>
+    {/each}
+  </Select>
+
+  <span class='add-goal'>
+    <Button on:click={() => console.log('Add a Goal')}>
+      <Icon class="material-icons">add</Icon>
+      <Label>Add a Goal</Label>
+    </Button>
+  </span>
+</span>
+
+{#if value === 'Daily'}
+  <Daily />
+{:else if value === 'Weekly'}
+  <p>Weekly</p>
+{:else}
+  <p>Monthly</p>
+{/if}
+
+<style>
+  .header {
+    font-size: 40px;
+    font-weight: 'bold';
+    padding-bottom: 20px;
+    font-family: 'Roboto'
+  }
+
+  .add-goal {
+    float: right;
+  }
+</style>
