34c92e2788d99408e33be7a69c4dcae4791143ed
Andrew <justincase25xxv@gmail.com>
2022-05-12 19:49:37 -0400

    Goals pt1 (#4)
    * Goals Select and Add button
    
    add padding to layout content
    
    select, button, and some styles
    
    base daily goals component
    
    * run prepare

---
+++ src/components/GoalsCalendar/Daily.svelte
@@ -0,0 +1,56 @@
+<div class='container'>
+  <div class='todo'>
+    <div class='col-header'>Todo</div>
+
+    <div class='goals-container'>
+    </div>
+  </div>
+
+  <div class='in-progress'>
+    <div class='col-header'>In Progress</div>
+
+    <div class='goals-container'>
+    </div>
+  </div>
+
+  <div class='done'>
+    <div class='col-header'>Done</div>
+
+    <div class='goals-container'>
+    </div>
+  </div>
+</div>
+
+<style>
+  .container {
+    padding-top: 20px;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-auto-columns: 33%;
+    grid-gap: 10px;
+  }
+
+  .col-header {
+    text-align: center;
+    font-family: 'Roboto'
+  }
+
+  .goals-container {
+    height: 100%;
+    border-radius: 5%;
+    border: white solid 1px;
+  }
+
+  .todo {
+    grid-column: 1 / 2;
+  }
+
+  .in-progress {
+    grid-column: 2 / 3;
+  }
+
+  .done {
+    grid-column: 3 / 4;
+  }
+</style>
