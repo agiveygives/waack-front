0fad65c3e5f82077f47d974413444430b963a5a1
Ashley Parker <aparker@bignerdranch.com>
2022-05-12 20:16:57 -0400

    Toggle sidebar open

--- src/routes/main/nav/__layout.svelte
+++ src/routes/main/nav/__layout.svelte
@@ -2,27 +2,39 @@
 	import { goto } from '$app/navigation';
 
 	import IconButton from '@smui/icon-button';
+	import Button from '@smui/button';
+	import { Icon, Label } from '@smui/common';
+	let open = false;
+	const toggleOpen = () => {
+		open = !open;
+	};
 </script>
 
 <div class="nav">
-	<div class="sidebar">
-		<IconButton class="material-icons" aria-label="goals" on:click={() => goto('/main/nav/goals')}
-			>pie_chart_outlined</IconButton
-		>
+	<div class={open ? 'sidebar-open' : 'sidebar'}>
+		<div class="sidebar-nav">
+			<Button on:click={() => goto('/main/nav/goals')} style="padding-left: 0">
+				<Icon class="material-icons">pie_chart_outlined</Icon>
+				<Label class="label">Goals</Label>
+			</Button>
+			<Button on:click={() => goto('/main/nav/accomplishments')} style="padding-left: 0">
+				<Icon class="material-icons">list_alt</Icon>
+				<Label>Accomplishments</Label>
+			</Button>
+			<Button on:click={() => goto('/main/nav/trophies')}>
+				<Icon class="material-icons">emoji_events_outlined</Icon>
+				<Label>Trophy Case</Label>
+			</Button>
+			<Button on:click={() => goto('/main/nav/career')} style="padding-left: 0">
+				<Icon class="material-icons arrow">arrow_circle_up</Icon>
+				<Label>Tech Career Progression</Label>
+			</Button>
+		</div>
 		<IconButton
-			class="material-icons"
-			aria-label="accomplishments"
-			on:click={() => goto('/main/nav/accomplishments')}>list_alt</IconButton
-		>
-		<IconButton
-			class="material-icons"
-			aria-label="trophies"
-			on:click={() => goto('/main/nav/trophies')}>emoji_events</IconButton
-		>
-		<IconButton
-			class="material-icons arrow"
-			aria-label="career"
-			on:click={() => goto('/main/nav/career')}>arrow_circle_up</IconButton
+			class="material-icons toggle"
+			aria-label="goals"
+			on:click={toggleOpen}
+			style={open ? 'transform: rotate(-180deg)' : ''}>arrow_forward_ios</IconButton
 		>
 	</div>
 
@@ -38,11 +50,28 @@
     justify-content: left;
     height: 100%;
 	}
-	.sidebar {
+	.sidebar,
+	.sidebar-open,
+	.sidebar-nav {
 		display: flex;
 		flex-direction: column;
+	}
+	.sidebar-nav {
+		align-items: flex-start;
+	}
+	.sidebar,
+	.sidebar-open {
 		height: 100%;
 		background-color: #676778;
+		justify-content: space-between;
+		padding-bottom: 1px;
+		width: 45px;
+		transition: all 300ms;
+		overflow: hidden;
+	}
+	.sidebar-open {
+		width: 350px;
+		overflow: visible;
 	}
 	.content {
     padding: 50px 75px;
@@ -50,5 +79,16 @@
 	}
 	:global(.arrow) {
 		transform: rotate(45deg);
+	}
+	:global(.toggle) {
+		margin-bottom: 56px;
+		transition: all 300ms;
+	}
+	:global(.mdc-button .mdc-button__icon) {
+		width: 45px;
+		font-size: 24px;
+	}
+	:global(.label) {
+		white-space: nowrap;
 	}
 </style>
