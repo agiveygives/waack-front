2f7ab3393b93ad0414cb7efcb5588cff7cde458a
Ashley Parker <aparker@bignerdranch.com>
2022-05-13 11:19:14 -0400

    Add a store for login info (#6)

--- src/routes/main/nav/index.svelte
+++ src/routes/main/nav/index.svelte
@@ -1,1 +1,13 @@
-<h1>HOME</h1>
+<script>
+	import { userInfo } from '../../../store/login';
+
+	let /** @type {import('../../../types/user').User} */ userValue = {
+			name: '',
+			email: '',
+			title: '',
+			manager: ''
+		};
+	userInfo.subscribe((value) => (userValue = value));
+</script>
+
+<h1>Welcome, {userValue.name}</h1>
